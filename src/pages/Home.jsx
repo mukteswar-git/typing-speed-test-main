adba27c07c6efe49731bcad4fdf5df6304f51f9f
Mukteswar Tripathy <mukteswart5@gmail.com>
2026-05-26 19:14:42 +0530

    feat: add Test Controls section for mobile screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import TestControls from "../components/TestControls";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <TestControls />
     </div>
   );
 };
