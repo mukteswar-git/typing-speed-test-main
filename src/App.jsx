043b6987b661fd900b7c911365669d379a84aee7
Mukteswar Tripathy <mukteswart5@gmail.com>
2026-05-30 16:48:32 +0530

    refactor: rename Navbar to Topbar and move it from Home to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Home from "./pages/Home";
+import Topbar from "./components/Topbar"
 
 const App = () => {
   return (
     <div className="font-sora">
+      <Topbar />
       <Home />
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import Navbar from "../components/Navbar";
 import Paragraph from "../components/Paragraph";
 import TestControls from "../components/TestControls";
 import restartIcon from "../assets/images/icon-restart.svg"
@@ -6,7 +5,6 @@ import restartIcon from "../assets/images/icon-restart.svg"
 const Home = () => {
   return (
     <div className="flex flex-col items-center">
-      <Navbar />
       <TestControls />
       <Paragraph />
       <button className="bg-gray-600 px-2 py-1 rounded-lg flex gap-2 text-sm mt-2">Restart Test <img src={restartIcon} className="w-3" /></button>
