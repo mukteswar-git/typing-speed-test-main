812386e144ab71c997002efb0614621a867a5b24
Mukteswar Tripathy <mukteswart5@gmail.com>
2026-05-30 16:28:57 +0530

    feat: add restart button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import Navbar from "../components/Navbar";
 import Paragraph from "../components/Paragraph";
 import TestControls from "../components/TestControls";
+import restartIcon from "../assets/images/icon-restart.svg"
 
 const Home = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <Navbar />
       <TestControls />
       <Paragraph />
+      <button className="bg-gray-600 px-2 py-1 rounded-lg flex gap-2 text-sm mt-2">Restart Test <img src={restartIcon} className="w-3" /></button>
     </div>
   );
 };
